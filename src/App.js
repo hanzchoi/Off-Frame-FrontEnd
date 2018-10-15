9668188cd8f9d3fa1e5ad9169f5a74378f6a2ab5
github username <personabe1984@gmail.com>
2018-10-15 13:22:55 -0400

    Added Semantic-React-Ui and made a route and container for the home page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
 import HomeContainer from './containers/HomeContainer'
-import logo from './logo.svg';
-import './App.css';
-import { Route } from 'react-router-dom'
+//import logo from './logo.svg';
+//import './App.css';
+import { Route, NavLink, Switch, Redirect} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <p>Hello</p>
-        </header>
-        <HomeContainer/>
+
+        <Switch>
+          <Route path='/' exact component={HomeContainer}/>
+          {//<Route component={}/> this is the default when there are no pages found
+          //<Route path='/product/:id'/>
+          }
+
+        </Switch>
       </div>
     );
   }
 }
 export default App;
+// the home is the default
+
+
+// <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+//   <p>Hello</p>
+// </header>
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -4,7 +4,10 @@ import React, { Component } from 'react';
 class HomeContainer extends Component {
   render() {
      return(
+       <div>
        <div> Home Container</div>
+       <p>What to do here?</p>
+       </div>
      )
   }
 }
