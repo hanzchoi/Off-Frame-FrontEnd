3beca5f5b6f76ded3d924345c7e0c8bffc6c2657
github username <personabe1984@gmail.com>
2018-10-15 10:16:14 -0400

    Setting up the routes and redux after some research

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import HomeContainer from './containers/HomeContainer'
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
